4df8672d4b3db5c3b687b9c2e3798d71cf47f30f
Kevin Constantin <kconstantin@protonmail.com>
2026-02-01 21:46:30 -0800

    add group photo on landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -40,6 +40,10 @@ onMounted(async () => {
       <img src="/flyer_open_house_2026.png" alt="Spokane Taiko performance" class="hero-image" />
     </div>
 
+    <div class="hero-photo">
+      <img src="/group_photo_outside.jpeg" alt="Spokane Taiko members" class="hero-image" />
+    </div>
+
     <div v-if="loading" class="loading">Loading announcements...</div>
 
     <div v-else-if="error" class="error">{{ error }}</div>
